bf3f12f032a3f9cbfe8c65e79102c3c41d67f62e
GUSTAVO FERNANDO DE FRAZÃO LIMA <48065631+GustavoFFLima@users.noreply.github.com>
2022-02-15 12:41:15 -0300

    Add files via upload

---
+++ 1_inicio/4_bloco.js
@@ -0,0 +1,11 @@
+// {} defini o começo e final de um bloco de código
+{
+    console.log("Passo #03");
+    console.log("Passo #02");
+    console.log("Passo #01");
+}
+
+{
+    console.log("Passo #00");
+    console.log("Passo #-01");
+}
