e84dfdd9447f73c8b1084b72e0e8c7f441e04e45
GustavlFFLima <frazao39@gmail.com>
2022-02-15 13:32:31 -0300

    incluir 7-11

---
+++ 1_inicio/10_desafio_area.js
@@ -0,0 +1,8 @@
+//Pi * raio * raio
+
+let raio = 10;
+const PI = 3.141592;
+let circunferencia;
+circunferencia = PI * raio * raio;
+console.log("O valor da área é " + circunferencia + "m2.");
+console.log(Math.PI * raio * raio);
