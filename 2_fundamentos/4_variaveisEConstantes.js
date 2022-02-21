7c0cdb8b42a3c6a2075c41c7819de9398970efb3
GUSTAVO FERNANDO DE FRAZÃO LIMA <48065631+GustavoFFLima@users.noreply.github.com>
2022-02-21 15:29:19 -0300

    Create 4_variaveisEConstantes.js

---
+++ 2_fundamentos/4_variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3;
+let b = 3; //mais moderna de se usar
+
+var a = 30
+ b = 40 // let n pode se re declarar
+
+console.log(a, b);
+
+a = 300
+b = 400
+
+console.log(a, b);
+
+const c = 5;
+// c = 50
+console.log(c)
