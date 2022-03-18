3e11810ba4687858043aa461e0e1d2aa852ac589
GUSTAVO FERNANDO DE FRAZÃO LIMA <48065631+GustavoFFLima@users.noreply.github.com>
2022-03-18 09:23:18 -0300

    Add files via upload

---
+++ 4_funcao/22_iife.js
@@ -0,0 +1,7 @@
+// IIFE -> Immediately Invoked Function Expression
+// invoca imediatamente, sem mexer no escopo glabal
+
+(function() {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
