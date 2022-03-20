dc80a1b2fce26371cd9c7e649e40fc136cfee3ce
GUSTAVO FERNANDO DE FRAZÃO LIMA <48065631+GustavoFFLima@users.noreply.github.com>
2022-03-20 19:22:53 -0300

    Add files via upload

---
+++ 6_objeto/1_oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento()  // Foco passou a  ser o objeto
+
+// Príncipios importantes:
+// 1. abstracao
+// 2. encassulamento
+// 3. herança (prototype)
+// 4. polimorfismo
