560a546ffa436fd0e605648dd988d26b7f1e673c
GUSTAVO FERNANDO DE FRAZÃO LIMA <48065631+GustavoFFLima@users.noreply.github.com>
2022-03-21 08:15:09 -0300

    Add files via upload

---
+++ 6_objeto/5_notacaoLiteral.js
@@ -0,0 +1,27 @@
+const a = 1
+const b = 2
+const c = 3
+
+const objt1 = {a: a, b:b, c: c}
+const objt2 = {a, b, c}
+console.log(objt1, objt2)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const objt3 = {}
+objt3 [nomeAttr] = valorAttr
+console.log(objt3)
+
+const objt4 = {[nomeAttr]: valorAttr}
+console.log(objt4)
+
+const obj5 = {
+    funcao1: function() {
+        //...
+    },
+    funcao2(){
+        //...
+    }
+}
+console.log(obj5)
